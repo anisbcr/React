966f6da0364be71d1069ab3cf60d04cc621260e0
anisbcr <zaki.game.hrachi@gmail.com>
2024-10-12 21:39:37 +0100

    Code213

---
+++ mongoDb/kursty krab/employee.js
@@ -0,0 +1,14 @@
+db.krusty-krab.insertOne({
+    "employés": [
+      {
+        "nom": "SpongeBob SquarePants",
+        "poste": "Cuisinier",
+        "id_employé": "SB001",
+        "heures_par_semaine": 40,
+        "contact": {
+          "téléphone": "+987654321",
+          "email": "spongebob@krustykrab.com"
+        }
+      }
+    ]
+  });
---
+++ mongoDb/kursty krab/recipees.js
@@ -0,0 +1,10 @@
+db.krusty-krab.insertOne({
+    "recettes": [
+      {
+        "nom": "Krabby Patty",
+        "prix": 5.99,
+        "ingrédients": ["Pain", "Steak haché", "Laitue", "Fromage", "Cornichons"],
+        "description": "Le burger signature du Krusty Krab."
+      }
+    ]
+  });
